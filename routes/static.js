f6682c29a422fbea306f232f5059eb054b21bb7e
nrb2002 <tshibasubaron@yahoo.fr>
2025-09-13 08:30:36 +0100

    New update
    Index file created and route

--- routes/static.js
+++ routes/static.js
@@ -1,14 +1,14 @@
-const express = require('express');
-const router = express.Router();
+const express = require('express'); //Import Express
+const router = express.Router(); //Use the router functionality of Express
 
 // Static Routes
 // Set up "public" folder / subfolders for static files
-router.use(express.static("public"));
-router.use("/css", express.static(__dirname + "public/css"));
-router.use("/js", express.static(__dirname + "public/js"));
-router.use("/images", express.static(__dirname + "public/images"));
+router.use(express.static("public")); //Use express static resources
+router.use("/css", express.static(__dirname + "public/css")); //Any route that contains "/css" will refer to the public/css folder
+router.use("/js", express.static(__dirname + "public/js")); //Any route that contains "/css" will refer to the public/js folder
+router.use("/images", express.static(__dirname + "public/images")); //Any route that contains "/css" will refer to the public/images folder
 
-module.exports = router;
+module.exports = router; //Export the route object for use in other areas of the application
 
 
 
--- server.js
+++ server.js
@@ -6,14 +6,33 @@
  * Require Statements
  *************************/
 const express = require("express")
+const expressLayouts = require("express-ejs-layouts") //Import Express EJS Layouts
 const env = require("dotenv").config()
 const app = express()
 const static = require("./routes/static")
+const expressEjsLayouts = require("express-ejs-layouts")
 
 /* ***********************
  * Routes
  *************************/
 app.use(static)
+
+//Index route
+//Express app watches the get object within the http request, identifies the "/" for the index or default route
+app.get("/", function(req, res){ //function that handles the request and the response objects
+  res.render("index", {title:"Home"}) //The response returned to the browser is the index page with the tile of Home
+})
+
+/* ***********************
+ * View Engine and Templates
+ *************************/
+app.set("view engine", "ejs") //Set EJS as the view engine
+app.use(expressEjsLayouts) //Tell the application to use EJS
+app.set("layout", "./layouts/layout") //Direct the application to look for EJS template views in the layouts folder
+
+
+
+
 
 /* ***********************
  * Local Server Information
